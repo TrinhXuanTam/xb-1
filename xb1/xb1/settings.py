f7e4d35eab6ed9e4775a5f55acdbcfeca6c00ea8
Daniel Ridzoň <ridzodan@fit.cvut.cz>
2020-03-14 23:14:03 +0100

    Update xb1/xb1/settings.py

--- xb1/xb1/settings.py
+++ xb1/xb1/settings.py
@@ -72,9 +72,9 @@ TEMPLATES = [
         'OPTIONS': {
             'debug': DEBUG,
             'loaders': [
+                'admin_tools.template_loaders.Loader',
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
-                'admin_tools.template_loaders.Loader',
             ],
             'context_processors': [
                 'django.template.context_processors.debug',
