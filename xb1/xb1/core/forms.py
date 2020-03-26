711828e6050e9dc0aa49f4303277d46392cffe1d
trinhxu2 <trinhxu2@fit.cvut.cz>
2020-03-26 13:37:52 +0100

    registration page design update

--- xb1/xb1/core/forms.py
+++ xb1/xb1/core/forms.py
@@ -6,12 +6,20 @@ from .models import User
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
 
-    # remove helper text
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
-        for field_name in ('username', 'email', 'password1', 'password2'):
-            self.fields[field_name].help_text = None
 
+        for field_name in ('username', 'email', 'password1', 'password2'):
+            # remove helper text
+            self.fields[field_name].help_text = None
+            # remove labels
+            self.fields[field_name].label = False;
+
+        # set text placeholders
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['email'].widget.attrs['placeholder'] = 'E-mail'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Retype password'
 
     class Meta:
         model = User
