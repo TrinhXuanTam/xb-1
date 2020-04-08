930eb14b4f76abbf7e1ae7811ea48d60166eb4c7
Daniel Ridzoň <ridzodan@fit.cvut.cz>
2020-04-08 22:18:26 +0200

    Added type choices to shop item

--- xb1/xb1/eshop/models.py
+++ xb1/xb1/eshop/models.py
@@ -3,10 +3,22 @@ from django.db import models
 # Create your models here.
 
 class ShopItem(models.Model):
+
+	TOP = 0
+	SALE = 1
+	NONE = 2
+	NEW = 3
+
+	TYPE_CHOICES = (
+		(TOP, "Top"),
+		(SALE, "Sale"),
+		(NONE, "None"),
+		(NEW, "New")
+	)
 	
 	itemName = models.CharField("Name", max_length = 20)
 	itemPrice = models.CharField("Price", max_length = 10)
-	#itemImg -> Odkaz nebo Pure data ?
+	#itemImg = TODO - will be implemented later
 	itemDesc = models.CharField("Description", max_length = 200);
-	#itemType -> TOP/SALE/NONE/NEW -> radioButton
+	itemType = models.PositiveSmallIntegerField("Item type", choices=TYPE_CHOICES, default=NONE)
 	itemActive = models.BooleanField("Active", default=True);
