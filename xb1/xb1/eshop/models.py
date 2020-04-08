b1cac17c56c1bd6ba1c354999fca2060278b3327
farget93 <tomkovarik@email.cz>
2020-04-08 22:05:49 +0200

    Base of ShopItem model added, require future work

--- xb1/xb1/eshop/models.py
+++ xb1/xb1/eshop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ShopItem(models.Model):
+	
+	itemName = models.CharField("Name", max_length = 20)
+	itemPrice = models.CharField("Price", max_length = 10)
+	#itemImg -> Odkaz nebo Pure data ?
+	itemDesc = models.CharField("Description", max_length = 200);
+	#itemType -> TOP/SALE/NONE/NEW -> radioButton
+	itemActive = models.BooleanField("Active", default=True);
