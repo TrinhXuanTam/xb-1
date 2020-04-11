b4b7db32d72eb5f573c3e3f4cb481954bf6635c4
ridzodan <ridzodan@fit.cvut.cz>
2020-04-11 18:45:48 +0200

    Updated: Missing password file should not cause errors.

--- initial_web/xb1/xb1/settings.py
+++ initial_web/xb1/xb1/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from passwords import EMAIL_PASSWORD
+
+try:
+    from passwords import EMAIL_PASSWORD
+except ModuleNotFoundError:
+    EMAIL_PASSWORD = ""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
