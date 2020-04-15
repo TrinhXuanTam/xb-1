272d5950820a2856fde3e4270a2631bbd45c8f05
Daniel Ridzoň <ridzodan@fit.cvut.cz>
2020-04-15 15:30:00 +0200

    pridat-date-time-picker

--- xb1/xb1/articles/forms.py
+++ xb1/xb1/articles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from .models import Animal
+from .models import Animal, Article
+from ..core.widgets import DateTimePickerInput
 
 
 class AnimalForm(forms.ModelForm):
@@ -19,3 +20,25 @@ class AnimalForm(forms.ModelForm):
             self.add_error("name", "Name cannot be dog.")
 
         return cleaned_data
+
+
+class ArticleForm(forms.ModelForm):
+
+    class Meta:
+        forms.DateTimeField
+        model = Article
+        fields = [
+            "title", "thumbnail", "text", "slug", "category", "tags",
+            "allow_comments", "sources", "article_state", "published_from",
+            "published_to"
+        ]
+    
+    def __init__(self, *args, **kwargs):
+
+        super(ArticleForm, self).__init__(*args, **kwargs)
+
+        self.fields["published_from"].widget = DateTimePickerInput()
+        self.fields["published_from"].widget.attrs["input_formats"] = ['%d/%m/%Y %H:%M']
+
+        self.fields["published_to"].widget = DateTimePickerInput()
+        self.fields["published_to"].widget.attrs["input_formats"] = ['%d/%m/%Y %H:%M']
