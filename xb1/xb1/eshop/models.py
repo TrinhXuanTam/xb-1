a1e8b7cac0b8d4171118cc2e1597d8edfbf6d251
farget93 <tomkovarik@email.cz>
2020-04-21 21:15:24 +0200

    Add Image Field to ShopItem

--- xb1/xb1/eshop/models.py
+++ xb1/xb1/eshop/models.py
@@ -16,7 +16,7 @@ class ShopItem(models.Model):
 	
 	itemName = models.CharField("Name", max_length = 20)
 	itemPrice = models.DecimalField("Price", decimal_places=2, max_digits=10)
-	#itemImg = TODO - will be implemented later
+	itemImg = models.ImageField("Image", default='default.jpg', null=True, blank=True, upload_to='ShopItems')
 	itemDesc = models.CharField("Description", max_length = 200);
 	itemType = models.PositiveSmallIntegerField("Item type", choices=TYPE_CHOICES, default=NONE)
 	itemActive = models.BooleanField("Active", default=True);
