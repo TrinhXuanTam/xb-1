63af933582b6c720273656381f8352fd5596044a
ridzodan <ridzodan@fit.cvut.cz>
2020-04-23 14:32:19 +0200

    Updated: Fixed merge conflicts.

--- xb1/xb1/articles/models.py
+++ xb1/xb1/articles/models.py
@@ -5,9 +5,12 @@ from django.utils.translation import ugettext_lazy as _
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 from os.path import basename, splitext
+from django.utils.text import slugify
 
 
 from ..core.models import User
+
+import random
 
 
 class Category(models.Model):
@@ -70,6 +73,22 @@ class Article(TimeStampedModel):
     text           = RichTextField(verbose_name=_("Text"), blank=True, null=True)
     sources        = models.TextField(verbose_name=_("Sources"), blank=True, null=True)
     article_state  = models.PositiveSmallIntegerField(verbose_name=_("State"), choices=STATE_CHOICES, default=HIDDEN)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self._generate_slug()
+
+        super().save(*args, **kwargs)
+
+    def _generate_slug(self):
+
+        slg = slugify(self.title)
+
+        # Test if slug exists
+        while Article.objects.filter(slug=slg).exists():
+            slg = slg + "_" + str(random.randint(0, 1000))
+
+        self.slug = slg
 
 
 class ForumCategory(models.Model):
