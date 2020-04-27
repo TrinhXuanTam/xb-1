5712f1a8208371857b73433f5ba0b3974fc42acc
ridzodan <ridzodan@fit.cvut.cz>
2020-04-27 18:27:34 +0200

    Updated: Added permissions to articles.

--- xb1/xb1/articles/views.py
+++ xb1/xb1/articles/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.utils import timezone
@@ -20,23 +20,25 @@ class ArticleListView(LoginMixinView, ListView):
     template_name = "articles.html"
 
 
-class ArticleCreateView(LoginMixinView, LoginRequiredMixin, CreateView):
+class ArticleCreateView(LoginMixinView, LoginRequiredMixin, PermissionRequiredMixin, CreateView):
 
     template_name = "articles_form.html"
     form_class = ArticleForm
     success_url = reverse_lazy("articles:article_list")
+    permission_required = "articles.add_article"
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(ArticleCreateView, self).form_valid(form)
 
 
-class ArticleUpdateView(LoginMixinView, LoginRequiredMixin, UpdateView):
+class ArticleUpdateView(LoginMixinView, LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
 
     model = Article
     template_name = "articles_form.html"
     form_class = ArticleForm
     success_url = reverse_lazy("articles:article_list")
+    permission_required = "articles.change_article"
 
     def form_valid(self, form):
         form.instance.author = self.request.user
