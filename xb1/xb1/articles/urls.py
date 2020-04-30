eb3e01cfefc9d0ba87d2b1f41d5f316aa36bb3f3
trinhxu2 <trinhxu2@fit.cvut.cz>
2020-04-30 11:29:55 +0200

    added comment reply functionality

--- xb1/xb1/articles/urls.py
+++ xb1/xb1/articles/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/', views.ArticleUpdateView.as_view(), name="article_update"),
     url(r'^(?P<slug>[\w-]+)/$', views.ArticleDetailView.as_view(), name="detail"),
     url(r'^post_comment', login_required(views.PostCommentView.as_view()), name="post_comment"),
+    url(r'^post_reply', login_required(views.PostCommentReplyView.as_view()), name="post_reply"),
     
 ]
--- xb1/xb1/articles/views.py
+++ xb1/xb1/articles/views.py
@@ -58,12 +58,12 @@ class ArticleDetailView(LoginMixinView, DetailView):
         article = Article.objects.get(slug=kwargs['slug'])
         comments = None
         if article.allow_comments:
-            comments = Comment.objects.filter(article=article.id, reaction_to_id=None)
+            comments = Comment.objects.filter(article=article.id, reaction_to_id=None).order_by('date').reverse()
             # Get comments with user data
             for comment in comments:
                 comment.user = Profile.objects.get(user_id=comment.author_id)
                 # Get replies to given comment
-                comment.replies = Comment.objects.filter(reaction_to_id=comment.id)
+                comment.replies = Comment.objects.filter(reaction_to_id=comment.id).order_by('date').reverse()
                 for reply in comment.replies:
                     reply.user = Profile.objects.get(user_id=reply.author_id)
         context['article'] = article
@@ -77,10 +77,28 @@ class ArticleDetailView(LoginMixinView, DetailView):
 class PostCommentView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            new_comment = Comment(text=request.POST.get('text'), author_id=request.user.id, article_id=int(request.POST.get('article')))
+            article_id = int(request.POST.get('article'))
+            comment_text = request.POST.get('text')
+            new_comment = Comment(text=comment_text, author_id=request.user.id, article_id=article_id)
             new_comment.save()
             new_comment.user = Profile.objects.get(user_id=request.user.id)
-            return render(request, 'new_comment.html', {"comment":new_comment})
+            context = {"comment":new_comment, "article": Article.objects.get(id=article_id)}
+            return render(request, 'new_comment.html', context)
+        else:
+            response = JsonResponse({"error": "Unauthorized"})
+            response.status_code = 401
+            return response
+
+class PostCommentReplyView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            reply_text = request.POST.get('text')
+            article_id = int(request.POST.get('article'))
+            reaction_id = int(request.POST.get('comment_id'))
+            new_reply = Comment(text=reply_text, author_id=request.user.id, article_id=article_id, reaction_to_id=reaction_id)
+            new_reply.save()
+            new_reply.user = Profile.objects.get(user_id=request.user.id)
+            return render(request, 'new_reply.html', {"reply":new_reply})
         else:
             response = JsonResponse({"error": "Unauthorized"})
             response.status_code = 401
