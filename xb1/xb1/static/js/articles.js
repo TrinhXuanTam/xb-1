f12a2fe20845bf6379fcf3d5e07350304c21f25e
trinhxu2 <trinhxu2@fit.cvut.cz>
2020-05-12 18:42:39 +0200

    slider menu update

--- xb1/xb1/static/js/articles.js
+++ xb1/xb1/static/js/articles.js
@@ -1,7 +1,28 @@
+var child_index = 0 
+var children_cnt = $('.articles_categories_wrapper').children().length
+
 $(".slider_next").click(function() {
-    $('.articles_categories_wrapper').animate({'margin-left':'-=190px'}, 200);
+    if (child_index > children_cnt - 2)
+        return
+
+    off = $('.articles_categories_wrapper').children().eq(child_index).innerWidth()
+    $('.articles_categories_wrapper').animate({'margin-left':'-=' + off + 'px'}, 200);
+    child_index += 1
 })
 
 $(".slider_back").click(function() {
-    $('.articles_categories_wrapper').animate({'margin-left':'+=190px'}, 200);
+    if (child_index == 0)
+        return
+    child_index -=1 
+    off = $('.articles_categories_wrapper').children().eq(child_index).innerWidth()
+    $('.articles_categories_wrapper').animate({'margin-left':'+=' + off + 'px'}, 200);
+})
+
+$(".article_category").click(function() {
+    if($(this).hasClass('article_category_active'))
+        return
+
+    $('.article_category').removeClass("article_category_active")
+    $(this).addClass('article_category_active')
+
 })
