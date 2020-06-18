0566cecd9f12c8c4bb7f36c6e45e68808274ee76
farget93 <tomkovarik@email.cz>
2020-06-19 01:03:32 +0200

    Basic login test

--- xb1/xb1/core/tests.py
+++ xb1/xb1/core/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.test import LiveServerTestCase
 
-# Create your tests here.
+from django.urls import reverse
+from unittest import skip
+
+from ..tests import SeleniumTestCase
+from .models import User
+
+import time
+
+class AuthenticationTests(SeleniumTestCase):
+	
+	@skip
+	def test_register(self):
+		self.wb.get('%s%s' % (self.live_server_url, reverse('eshop:shopIndex')))
+		print(User.objects.count())
+		time.sleep(2)
+
+	def test_login(self):
+		User.objects.create_user(username='admin', email='admin@admin.com', password='admin')
+			
+		print(User.objects.count())
+		
+		self.wb.get('%s%s' % (self.live_server_url, ''))
+		time.sleep(2)
+		
+		loginPopup = self.wb.find_element_by_xpath('/html/body/div[3]/ul[1]/li[1]/a')
+		loginPopup.click()
+		
+		time.sleep(1)
+		
+		nameField = self.wb.find_element_by_xpath('//*[@id="id_username"]')
+		nameField.send_keys('admin')
+		
+		passwordField = self.wb.find_element_by_xpath('//*[@id="id_password"]')
+		passwordField.send_keys('admin')
+		
+		loginButton = self.wb.find_element_by_xpath('//*[@id="login_form"]/button')
+		loginButton.click()
+		
+		time.sleep(2)
+		
+		logoutButton = self.wb.find_element_by_xpath('/html/body/div[3]/ul[2]/li/a')
+		self.assertNotEqual(logoutButton, None)
---
+++ xb1/xb1/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+
+from selenium import webdriver
+
+from .settings import WEB_DRIVER_LOCATION
+
+class SeleniumTestCase(StaticLiveServerTestCase):
+
+	def setUp(self):
+		self.wb = webdriver.Chrome(WEB_DRIVER_LOCATION)
+		super(StaticLiveServerTestCase, self).setUp()
+		
+	def tearDown(self):
+		#self.wb.close()
+		super(StaticLiveServerTestCase, self).tearDown()
