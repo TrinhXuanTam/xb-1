ab212451e1ca7ab609af455f26ca047fefcc097c
farget93 <tomkovarik@email.cz>
2020-06-21 16:05:24 +0200

    Eshop code commented

--- xb1/xb1/eshop/tests.py
+++ xb1/xb1/eshop/tests.py
@@ -1,15 +1,3 @@
 from django.test import TestCase
 from django.test import LiveServerTestCase
 from django.urls import reverse
-
-from selenium import webdriver
-
-from ..settings import WEB_DRIVER_LOCATION
-
-#class Test(SeleniumTestCase):
-#
-#	def test_X(self):
-#		print(self.live_server_url)
-#		WEB_DRIVER = webdriver.Chrome(WEB_DRIVER_LOCATION)
-#		WEB_DRIVER.get('http://www.seznam.cz')
-	
