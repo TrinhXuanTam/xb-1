70964189805a9e0482e535d354861ee162b79f54
ridzodan <ridzodan@fit.cvut.cz>
2020-06-24 10:09:31 +0200

    Fixed tests.

--- xb1/xb1/core/tests.py
+++ xb1/xb1/core/tests.py
@@ -53,12 +53,12 @@ class AuthenticationTests(SeleniumTestCase):
 		self.assertNotEqual(user, None)
 
 		self.wb.get('%s%s' % (self.live_server_url, reverse('activate_registration', kwargs={'uidb64': urlsafe_base64_encode(force_bytes(user.pk)), 'token': account_activation_token.make_token(user)})))
-		
+
 		try:
 			self.wb.find_element_by_xpath('/html/body/div[3]/ul[2]/li/a')
 		except NoSuchElementException:
 			self.fail("Login failed")
-	
+
 	def test_login(self):
 		User.objects.create_user(username='admin', email='admin@admin.com', password='admin')
 		result = super().login('admin', 'admin')
@@ -67,15 +67,15 @@ class AuthenticationTests(SeleniumTestCase):
 	def test_change_profile(self):
 		user = User.objects.create_user(username='admin', email='admin@admin.com', password='admin')
 		Profile.objects.create(user=user)
-		
+
 		result = super().login('admin', 'admin')
 		self.assertEqual(result, 0)
-		
+
 		profileLink = self.wb.find_element_by_xpath('/html/body/div[3]/ul[1]/li[1]/a')
 		profileLink.click()
-		
+
 		time.sleep(2)
-		
+
 		nickname = 'XXX'
 		firstname = 'YYY'
 		lastname = 'ZZZ'
@@ -84,53 +84,53 @@ class AuthenticationTests(SeleniumTestCase):
 		address = '5525'
 		phoneNumber = '777000777'
 		imageLocation = '/resource/test/profileImage.png'
-		
+
+		imageField = self.wb.find_element_by_xpath('//*[@id="id_image"]')
+		imageField.send_keys(BASE_DIR + imageLocation)
+
 		nicknameField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/input[4]')
 		nicknameField.send_keys(nickname)
-		
+
 		firstnameField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/input[5]')
 		firstnameField.send_keys(firstname)
-		
+
 		lastnameField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/input[6]')
 		lastnameField.send_keys(lastname)
-		
+
 		cityField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/input[7]')
 		cityField.send_keys(city)
-		
+
 		postNumberField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/input[8]')
 		postNumberField.send_keys(postNumber)
-		
+
 		addressField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/input[9]')
 		addressField.send_keys(address)
-		
+
 		phoneNumberField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/input[10]')
 		phoneNumberField.send_keys(phoneNumber)
-		
-		imageField = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/div/input')
-		imageField.send_keys(BASE_DIR + imageLocation)
 
 		confirmButton = self.wb.find_element_by_xpath('/html/body/div[4]/div/div[2]/form/div/button')
 		confirmButton.click()
-		
+
 		time.sleep(2)
-		
+
 		nicknameField = self.wb.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/input[4]')
 		self.assertEqual(nicknameField.get_attribute('value'), nickname)
-		
+
 		firstnameField = self.wb.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/input[5]')
 		self.assertEqual(firstnameField.get_attribute('value'), firstname)
-		
+
 		lastnameField = self.wb.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/input[6]')
 		self.assertEqual(lastnameField.get_attribute('value'), lastname)
-		
+
 		cityField = self.wb.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/input[7]')
 		self.assertEqual(cityField.get_attribute('value'), city)
-		
+
 		postNumberField = self.wb.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/input[8]')
 		self.assertEqual(postNumberField.get_attribute('value'), postNumber)
-		
+
 		addressField = self.wb.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/input[9]')
 		self.assertEqual(addressField.get_attribute('value'), address)
-		
+
 		phoneNumberField = self.wb.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/input[10]')
 		self.assertEqual(phoneNumberField.get_attribute('value'), phoneNumber)
