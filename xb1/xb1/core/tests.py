23037f8dba09bcf831343ea7d4084a0aca16989c
farget93 <tomkovarik@email.cz>
2020-10-03 13:37:47 +0200

    Login test

--- xb1/xb1/core/tests.py
+++ xb1/xb1/core/tests.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 from unittest import skip
 
-from ..tests import SeleniumTestCase
+from ..tests import SeleniumAuthenticatedTestCase
 from ..settings import BASE_DIR
 from .models import User
 from .models import Profile
@@ -21,16 +21,10 @@ from ..driver import getDriver
 
 import time
 
-class AuthenticationTests(SeleniumTestCase):
+class AuthenticationTests(SeleniumAuthenticatedTestCase):
 
 	def test_register(self):
-		driver = getDriver()
-		driver.get("http://www.seznam.cz")
-		
-		time.sleep(2)
-		
-		element = driver.find_element_by_xpath('//*[@id="hp-app"]/div/div[1]/div[2]/div/div/div[1]/header/div/div/div[2]/div[1]/form/div/label/input')
-		
-		time.sleep(2)
-		driver.quit()
-		print("XXX")
+		pass
+	
+	def test_login(self):
+		super().login('admin', 'admin')
--- xb1/xb1/tests.py
+++ xb1/xb1/tests.py
@@ -1,20 +1,35 @@
+import time
+
+from .driver import getDriver
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase
 from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-import time
-
 class SeleniumTestCase(StaticLiveServerTestCase):
 
 	def setUp(self):
+		self.wb = getDriver()
 		super(StaticLiveServerTestCase, self).setUp()
 
 	def tearDown(self):
+		self.wb.quit()
 		super(StaticLiveServerTestCase, self).tearDown()
+		
+class SeleniumAuthenticatedTestCase(SeleniumTestCase):
 
-	def login(self):
-		return 0
+	def login(self, name, password):
+		self.wb.get('%s%s' % (self.live_server_url, reverse('index')))
+		time.sleep(1)
+		loginPopup = self.wb.find_element_by_xpath('/html/body/div[3]/ul[1]/li[1]/a')
+		loginPopup.click()
+		time.sleep(1)
+		nameField = self.wb.find_element_by_xpath('//*[@id="id_username"]')
+		nameField.send_keys(name)
+		passwordField = self.wb.find_element_by_xpath('//*[@id="id_password"]')
+		passwordField.send_keys(password)
+		loginButton = self.wb.find_element_by_xpath('//*[@id="login_form"]/button')
