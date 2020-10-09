c38c73a3ac0021e9f5b7ec6b4eb11a01ae262c01
Tomáš Kovářík <kovart12@fit.cvut.cz>
2020-10-10 00:36:58 +0200

    Change of remote address

--- xb1/xb1/driver.py
+++ xb1/xb1/driver.py
@@ -4,4 +4,4 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def getDriver():
-	return webdriver.Remote(command_executor=os.getenv('SELENIUM_REMOTE_ADDRESS', 'http://selenium__standalone-chrome:4444/wd/hub'), desired_capabilities=DesiredCapabilities.CHROME)
+	return webdriver.Remote(command_executor=os.getenv('SELENIUM_REMOTE_ADDRESS', 'localhost:4444/wd/hub'), desired_capabilities=DesiredCapabilities.CHROME)
