d3f23f0ce0a41e25230dcc9b06d5e518f982a03d
Tomáš Kovářík <kovart12@fit.cvut.cz>
2020-11-30 22:06:00 +0100

    #116, #117 included here

--- xb1/xb1/shop/cart.py
+++ xb1/xb1/shop/cart.py
@@ -35,7 +35,8 @@ class Cart:
         return cart
 
     def insert(self, item):
-        models.CartEntry.objects.create(cart=self.cart, item=item, count=1, price=item.price)
+        specification = item.specification.entry.first() if item.specification else None
+        models.CartEntry.objects.create(cart=self.cart, item=item, count=1, price=item.price, specification=specification)
 
     def add(self, pk, count=1):
         entry = models.CartEntry.objects.filter(cart=self.cart, pk=pk).first()
