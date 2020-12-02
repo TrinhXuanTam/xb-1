9879e947b15c303f2a98c711ce5443d7d98f6d9e
Daniel Ridzoň <ridzodan@fit.cvut.cz>
2020-12-02 20:34:44 +0100

    Update production/start.sh

--- production/start.sh
+++ production/start.sh
@@ -2,3 +2,4 @@ sudo docker-compose up -d
 echo "Making database backup..."
 sudo docker-compose exec web python manage.py dumpdata --exclude=auth --exclude=contenttypes > "dumps/$(date +%F_%R).json"
 echo "Database backup finished"
+echo "Server is running, to view server log enter: sh log.sh"
