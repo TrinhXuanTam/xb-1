79771eba4943047c0172ce04ae217b499dfa4a28
ridzodan <ridzodan@fit.cvut.cz>
2020-12-02 19:04:59 +0100

    Backups now wont save unwanted tables.

--- production/start.sh
+++ production/start.sh
@@ -1,4 +1,4 @@
-sudo docker-compose up
+sudo docker-compose up -d
 echo "Making database backup..."
-sudo docker-compose exec web python manage.py dumpdata > "dumps/$(date +%F_%R).json"
+sudo docker-compose exec web python manage.py dumpdata --exclude=auth --exclude=contenttypes > "dumps/$(date +%F_%R).json"
 echo "Database backup finished"
--- production/update.sh
+++ production/update.sh
@@ -1,6 +1,6 @@
 sudo docker-compose up -d
 echo "Making database backup..."
-sudo docker-compose exec web python manage.py dumpdata > "dumps/$(date +%F_%R).json"
+sudo docker-compose exec web python manage.py dumpdata --exclude=auth --exclude=contenttypes > "dumps/$(date +%F_%R).json"
 echo "Database backup finished"
 sudo docker-compose stop
 sudo docker-compose pull web
