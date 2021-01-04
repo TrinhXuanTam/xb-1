9651f828fcf575968b003e45087b57455f8038d0
ridzodan <ridzodan@fit.cvut.cz>
2021-01-04 19:01:15 +0100

    Added user to collectstatic.

--- production/update.sh
+++ production/update.sh
@@ -1,3 +1,15 @@
+mkdir -p media
+mkdir -p dumps
+mkdir -p staticfiles
+
+chgrp docker media
+chgrp docker dumps
+chgrp docker staticfiles
+
+chmod g+w media
+chmod g+w dumps
+chmod g+w staticfiles
+
 docker-compose up -d
 echo "Making database backup..."
 docker-compose exec web python manage.py dumpdata --all --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 2 > "dumps/$(date +%F-%R).json"
@@ -6,6 +18,6 @@ docker-compose stop
 docker-compose pull web
 docker-compose up --build -d
 docker-compose exec web python manage.py migrate --no-input
-docker-compose exec web python manage.py collectstatic --no-input --clear
+docker-compose exec web python manage.py collectstatic --no-input --clear --user xb1
 docker-compose stop
 echo "Updating server has successfully finished."
