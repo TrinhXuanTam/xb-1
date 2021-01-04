5bded00c151ee12f5d88f0cbf42b72f719430b51
ridzodan <ridzodan@fit.cvut.cz>
2021-01-04 18:20:04 +0100

    Removed sudo from update script.

--- production/update.sh
+++ production/update.sh
@@ -1,11 +1,11 @@
-sudo docker-compose up -d
+docker-compose up -d
 echo "Making database backup..."
-sudo docker-compose exec web python manage.py dumpdata --all --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 2 > "dumps/$(date +%F-%R).json"
+docker-compose exec web python manage.py dumpdata --all --natural-foreign --natural-primary -e contenttypes -e auth.Permission --indent 2 > "dumps/$(date +%F-%R).json"
 echo "Database backup finished"
-sudo docker-compose stop
-sudo docker-compose pull web
-sudo docker-compose up --build -d
-sudo docker-compose exec web python manage.py migrate --no-input
-sudo docker-compose exec web python manage.py collectstatic --no-input --clear
-sudo docker-compose stop
+docker-compose stop
+docker-compose pull web
+docker-compose up --build -d
+docker-compose exec web python manage.py migrate --no-input
+docker-compose exec web python manage.py collectstatic --no-input --clear
+docker-compose stop
 echo "Updating server has successfully finished."
