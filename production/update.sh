c8bbfd5d58b335e848388a166e53817f6eaa2805
ridzodan <ridzodan@fit.cvut.cz>
2021-01-04 19:07:19 +0100

    Added user to collectstatic.

--- production/update.sh
+++ production/update.sh
@@ -18,6 +18,7 @@ docker-compose stop
 docker-compose pull web
 docker-compose up --build -d
 docker-compose exec web python manage.py migrate --no-input
-docker-compose exec --user xb1 web python manage.py collectstatic --no-input --clear
+docker-compose exec web whoami
+docker-compose exec web --user xb1 python manage.py collectstatic --no-input --clear
 docker-compose stop
 echo "Updating server has successfully finished."
